f3ed665c9eeb6b1dcfed83f3d400691ca4ae0932
hedhyw <hedhyw@yahoo.com>
2018-08-18 13:54:12 +0300

    Cosmetic change

--- drivers/asana.go
+++ drivers/asana.go
@@ -1,8 +1,8 @@
 package drivers
 
 import (
+	"fmt"
 	"net/http"
-  "fmt"
 
 	"github.com/danilopolani/gocialite/structs"
 	"golang.org/x/oauth2"
@@ -14,10 +14,10 @@ func init() {
 	registerDriver(asanaDriverName, AsanaDefaultScopes, AsanaUserFn, AsanaEndpoint, AsanaAPIMap, AsanaUserMap)
 }
 
-// DailyMotionEndpoint is the oAuth endpoint
+// AsanaEndpoint is the oAuth endpoint
 var AsanaEndpoint = oauth2.Endpoint{
-  AuthURL:  "https://app.asana.com/-/oauth_authorize",
-  TokenURL: "https://app.asana.com/-/oauth_token",
+	AuthURL:  "https://app.asana.com/-/oauth_authorize",
+	TokenURL: "https://app.asana.com/-/oauth_token",
 }
 
 // AsanaUserMap is the map to create the User struct
@@ -25,21 +25,21 @@ var AsanaUserMap = map[string]string{}
 
 // AsanaAPIMap is the map for API endpoints
 var AsanaAPIMap = map[string]string{
-	"endpoint":      "https://app.asana.com/api/1.0",
-	"userEndpoint":  "/users/me?opt_fields=id,name,email,photo",
+	"endpoint":     "https://app.asana.com/api/1.0",
+	"userEndpoint": "/users/me?opt_fields=id,name,email,photo",
 }
 
 // AsanaUserFn is a callback to parse additional fields for User
 var AsanaUserFn = func(client *http.Client, u *structs.User) {
-  userData := u.Raw["data"].(map[string]interface{})
-  u.ID = fmt.Sprintf("%.0f", userData["id"].(float64))
-  u.Email = userData["email"].(string)
-  u.FullName = userData["name"].(string)
+	userData := u.Raw["data"].(map[string]interface{})
+	u.ID = fmt.Sprintf("%.0f", userData["id"].(float64))
+	u.Email = userData["email"].(string)
+	u.FullName = userData["name"].(string)
 
 	// Set avatar
-  if (userData["photo"] != nil) { 
-	 u.Avatar = userData["photo"].(map[string]interface{})["image_1024x1024"].(string)
-  }
+	if userData["photo"] != nil {
+		u.Avatar = userData["photo"].(map[string]interface{})["image_1024x1024"].(string)
+	}
 }
 
 // AsanaDefaultScopes contains the default scopes
