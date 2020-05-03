02de0e5df234ad3d07ba2bf8b0b3fde8acce692c
Waleed Gadelkareem <gadelkareem@gmail.com>
2020-05-03 20:11:00 +0200

    read confiscated shitty json

--- drivers/linkedin.go
+++ drivers/linkedin.go
@@ -1,40 +1,102 @@
 package drivers
 
 import (
-	"net/http"
+    "net/http"
 
-	"github.com/danilopolani/gocialite/structs"
-	"golang.org/x/oauth2/linkedin"
+    "github.com/danilopolani/gocialite/structs"
+    "golang.org/x/oauth2/linkedin"
 )
 
 const (
-	linkedinDriverName = "linkedin"
+    linkedinDriverName = "linkedin"
 )
 
 func init() {
-	registerDriver(linkedinDriverName, LinkedInDefaultScopes, LinkedInUserFn, linkedin.Endpoint, LinkedInAPIMap, LinkedInUserMap)
+    registerDriver(linkedinDriverName, LinkedInDefaultScopes, LinkedInUserFn, linkedin.Endpoint, LinkedInAPIMap, LinkedInUserMap)
 }
 
 // LinkedInUserMap is the map to create the User struct
 var LinkedInUserMap = map[string]string{
-	"id":            "ID",
-	"vanityName":    "Username",
-	"firstName":     "FirstName",
-	"lastName":      "LastName",
-	"formattedName": "FullName",
-	"emailAddress":  "Email",
-	"pictureUrl":    "Avatar",
+    "id":            "ID",
+    "vanityName":    "Username",
+    "firstName":     "FirstName",
+    "lastName":      "LastName",
+    "formattedName": "FullName",
+    "emailAddress":  "Email",
+    "pictureUrl":    "Avatar",
 }
 
 // LinkedInAPIMap is the map for API endpoints
 var LinkedInAPIMap = map[string]string{
-	"endpoint":     "https://api.linkedin.com",
-	"userEndpoint": "/v2/me",
-	"emailEndpoint": "/v2/emailAddress?q=members&projection=(elements*(handle~))",
+    "endpoint":      "https://api.linkedin.com",
+    "userEndpoint":  "/v2/me",
+    "emailEndpoint": "/v2/emailAddress?q=members&projection=(elements*(handle~))",
 }
 
 // LinkedInUserFn is a callback to parse additional fields for User
-var LinkedInUserFn = func(client *http.Client, u *structs.User) {}
+var LinkedInUserFn = func(client *http.Client, u *structs.User) {
+    /*
+       {
+          "id":"REDACTED",
+          "firstName":{
+             "localized":{
+                "en_US":"Tina"
+             },
+             "preferredLocale":{
+                "country":"US",
+                "language":"en"
+             }
+          },
+          "lastName":{
+             "localized":{
+                "en_US":"Belcher"
+             },
+             "preferredLocale":{
+                "country":"US",
+                "language":"en"
+             }
+          },
+           "profilePicture": {
+               "displayImage": "urn:li:digitalmediaAsset:B54328XZFfe2134zTyq"
+           }
+       }
+    */
+    u.FirstName = u.FirstName["localized"][fmt.Sprintf("%s_%s", u.FirstName["preferredLocale"]["language"],
+        u.FirstName["preferredLocale"]["country"])]
+    u.LastName = u.LastName["localized"][fmt.Sprintf("%s_%s", u.LastName["preferredLocale"]["language"],
+        u.LastName["preferredLocale"]["country"])]
+
+    userData := u.Raw["data"].(map[string]interface{})
+    u.Avatar = userData["profilePicture"]["displayImage"]
+
+    // Retrieve email
+    req, err := client.Get(LinkedinAPIMap["endpoint"] + LinkedinAPIMap["emailEndpoint"])
+    if err != nil {
+        return
+    }
+    defer req.Body.Close()
+    /*
+       {
+           "handle": "urn:li:emailAddress:3775708763",
+           "handle~": {
+               "emailAddress": "hsimpson@linkedin.com"
+           }
+       }
+    */
+    type EmailRes struct {
+        Handle struct {
+            EmailAddress string `json:"emailAddress"`
+        } `json:"handle~"`
+    }
+    email := new(EmailRes)
+    err = json.NewDecoder(req.Body).Decode(&email)
+    if err != nil {
+        return
+    }
+
+    u.Email = email.Handle.EmailAddress
+
+}
 
 // LinkedInDefaultScopes contains the default scopes
 var LinkedInDefaultScopes = []string{"r_emailaddress", "r_liteprofile", "w_member_social"}
